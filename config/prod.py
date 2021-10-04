75f13f07bdb1481b25f4efba5be9b71fc027cca9
Payel <payel.bhunia0301@gmail.com>
2021-10-05 01:07:13 +0530

    fixed type2

--- config/prod.py
+++ config/prod.py
@@ -1,6 +1,12 @@
 import os
+import re
 
+
+uri = os.getenv("DATABASE_URL")  # or other relevant config var
+if uri.startswith("postgres://"):
+    uri = uri.replace("postgres://", "postgresql://", 1)
 DEBUG = False
 SECRET_KEY = 'kiran'
-SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+SQLALCHEMY_DATABASE_URI = uri
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
