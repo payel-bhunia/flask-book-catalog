dfe53bf240306aee62254254328210cd11bbdeb1
Payel <bhunia.payel0301@gmail.com>
2021-10-04 22:42:42 +0530

    fixed error

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
 from app import create_app, db  # from the app package __init__
 from app.auth.models import User
+from sqlalchemy import exec
 
 #if __name__ == '__main__':
 flask_app = create_app('prod')
 with flask_app.app_context():
     db.create_all()
-    if not User.query.filter_by(user_name='harry').first():
-        User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
-    flask_app.run()
+    try:
+        if not User.query.filter_by(user_name='harry').first():
+            User.create_user(user='harry', email='harry@hogwarts.com', password='secret')
+    except exec.IntegrityError:
+        flask_app.run()
 
 
